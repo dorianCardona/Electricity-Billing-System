5564eeadc32bb226f37b9def19d6d2b126f6d587
Dorian Cardona <dorian.cardona@unosquare.com>
2026-06-09 15:20:06 -0500

    Initial commit

---
+++ src/main/java/com/unosquare/electricity/billing/system/Conn.java
@@ -0,0 +1,21 @@
+package com.unosquare.electricity.billing.system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs", "root", "");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/unosquare/electricity/billing/system/ElectricityBillingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unosquare.electricity.billing.system;
+
+/**
+ *
+ * @author dorian.cardona
+ */
+public class ElectricityBillingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
